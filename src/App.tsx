ebde4896dbea831736fe5c167ea3ab96d9e3c350
sumit kumar <sumit1523@gmail.com>
2024-02-10 00:03:53 +0530

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import './globals.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className="text-7xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
